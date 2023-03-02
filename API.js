5a0ed2097278977e48bd71aadb95e3dc58bc25b9
Mariuspersona <126747289+Mariuspersona@users.noreply.github.com>
2023-03-02 07:50:53 +0200

    Create API.js

---
+++ API.js
@@ -0,0 +1,11 @@
+export const main_site = {
+
+    main: 'https://www.demoblaze.com/'
+	
+	}
+
+export const URL = {
+
+    product_page:"https://www.demoblaze.com/prod.html?idp_=1",
+    cart:"https://www.demoblaze.com/cart.html"
+    	}
