17c9548e60204597f83c3ff1250c51fcc140c4b8
Mariuspersona <126747289+Mariuspersona@users.noreply.github.com>
2023-03-02 08:11:30 +0200

    Create Locators.js

---
+++ Locators.js
@@ -0,0 +1,18 @@
+export const categories={
+    phones:"//i[contains(@class, 'Phones')]", 
+    laptops:"//i[contains(@class, 'Laptops')]", 
+    monitors:"//i[contains(@class, 'Monitors')]",   
+}
+
+export const phones={
+    galaxy_s6:"//a[@href='https://www.demoblaze.com/prod.html?idp_=1']", 
+   
+}
+export const laptops={
+    vaio_i5:"//a[@href='https://www.demoblaze.com/prod.html?idp_=8']", 
+   
+}
+export const monitors={
+    asus:"//a[@href='https://www.demoblaze.com/prod.html?idp_=14']", 
+   
+}
