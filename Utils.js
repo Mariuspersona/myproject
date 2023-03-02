9733b562cab6c7a8186d2bb3ff85b9550bb6f054
Mariuspersona <126747289+Mariuspersona@users.noreply.github.com>
2023-03-02 14:57:13 +0200

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -2,11 +2,11 @@ import { actionsUtils } from "../../Actions";
 
 static clickOn(locator, action) {      
 actionsUtils.click.command:
-this.#commandAndClick(locator, action);
+this.#commandAndClick(locator, action)
   
 }
 
-static #type(locator, string) {
-cy.typeString(locator, string)       
+static #typeStringAndEnter(locator, string) {
+cy.typeString(locator, string+'{enter}')       
   
 }
