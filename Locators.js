aa15dddd5623a870c30ba8720aae6f552ff6f774
Mariuspersona <126747289+Mariuspersona@users.noreply.github.com>
2023-03-02 14:20:02 +0200

    Update Locators.js

--- Locators.js
+++ Locators.js
@@ -1,22 +1,45 @@
 export const categories={
     phones:"//i[contains(@class, 'Phones')]", 
     laptops:"//i[contains(@class, 'Laptops')]", 
-    monitors:"//i[contains(@class, 'Monitors')]",   
+    monitors:"//i[contains(@class, 'Monitors')]"   
 }
 
 export const phones={
-    galaxy_s6:"//a[@href='https://www.demoblaze.com/prod.html?idp_=1']", 
+    galaxy_s6:"//a[@href='https://www.demoblaze.com/prod.html?idp_=1']" 
    
 }
 export const laptops={
-    vaio_i5:"//a[@href='https://www.demoblaze.com/prod.html?idp_=8']", 
+    vaio_i5:"//a[@href='https://www.demoblaze.com/prod.html?idp_=8']"
    
 }
 export const monitors={
-    asus:"//a[@href='https://www.demoblaze.com/prod.html?idp_=14']", 
+    asus:"//a[@href='https://www.demoblaze.com/prod.html?idp_=14']" 
  }   
     
 export const cart_button={
     cart_button:"//i[contains(@class, 'btn btn-success btn-lg')]"     
    
 }
+
+export const place_order={
+    place_order:"//button[text()='Place Order']"    
+   
+}
+
+export const cartbutton_header={
+    cartbutton_header:"//a[@href='https://www.demoblaze.com/cart.html']" 
+    
+}
+
+export const place_order={
+    name:"//input[@id='name']", 
+    country:"//input[@id='country']", 
+    city:"//input[@id='city']", 
+    credit_card:"//input[@id='card']",
+    month:"//input[@id='month']",
+    year:"//input[@id='year']"
+    
+}    
+    
+    
+    
